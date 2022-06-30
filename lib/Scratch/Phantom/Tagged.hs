dbfa8e958becab8bd4a92dc27a51b50544bd42cf
Zetrik <30878725+ztk37@users.noreply.github.com>
2022-06-30 21:38:34 +0200

    Update Tagged.hs

--- lib/Scratch/Phantom/Tagged.hs
+++ lib/Scratch/Phantom/Tagged.hs
@@ -1,5 +1,23 @@
 module Scratch.Phantom.Tagged where
 
+import Scratch.Phantom.Proxy
+import Data.Bifunctor
+
 data Tagged tag a = Tagged
     { unTagged :: a
     } deriving (Eq, Show)
+
+untag :: Tagged tag a -> a
+untag = unTagged
+
+retag :: Proxy tag' -> Tagged tag a -> Tagged tag' a
+retag _ (Tagged a) = (Tagged a)
+
+toProxy :: Tagged tag a -> Proxy tag
+toProxy _ = Proxy
+
+instance Functor (Tagged tag) where
+  fmap f = Tagged . f . unTagged
+
+instance Bifunctor Tagged where
+  bimap _ g = Tagged . g . unTagged
